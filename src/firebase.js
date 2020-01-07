942185de8eab3db7eb0c29c42426fb217cd79b5b
Ryley Randall <33163774+ryleyrandall22@users.noreply.github.com>
2020-01-06 22:48:26 -0700

    First Commit
    Set up app and deployed

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBgvli8kpWMJkIp5xVRnvMkA6w6D9qcHZw",
+  authDomain: "authentication3-a804f.firebaseapp.com",
+  databaseURL: "https://authentication3-a804f.firebaseio.com",
+  projectId: "authentication3-a804f",
+  storageBucket: "authentication3-a804f.appspot.com",
+  messagingSenderId: "626813518390",
+  appId: "1:626813518390:web:3db4080b26f1c7e0a88e97"
 };
 
 firebase.initializeApp(config);
